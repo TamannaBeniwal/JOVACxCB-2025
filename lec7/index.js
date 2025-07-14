03885df87afdefb4ec0b097497cf9b0adf6ecef3
tamanna <tamannabeniwal007@gmail.com>
2025-07-14 16:26:50 +0530

    Delete all files to make branch empty

--- lec7/index.js
+++
@@ -1,5 +0,0 @@
-//event delegation in javascript(it is a technique where you attach event listner only on a single parent,use to handle its child also )
-//why vent delegation? 1.cleaner code 2.better performance 
-
-const menuItem = document.getElementById("menuItems");
-menuItem.addEventListener("click",(event)=)
--- lec8nodejs/app.js
+++
@@ -1,9 +0,0 @@
-// var aa=20;
-// var bb=30;
-// console.log(aa+bb);
-
-// console.log(global);
-
-
-console.log(this);
-// console.log(window);
