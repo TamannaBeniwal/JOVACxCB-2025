3a919d9c028cf69e6f7d6a52a387f12684e8a458
tamanna <tamannabeniwal007@gmail.com>
2025-07-14 16:12:19 +0530

    Fresh start

---
+++ lec8nodejs/1.modules/app.js
@@ -0,0 +1,15 @@
+
+//main file
+
+
+console.log("this is my file");
+
+const {myName, sumOFTwoNumbers }=require("./sum.js");
+
+//console.log(obj.myName);
+console.log(myName);
+
+//obj.sumOfTwoNumbers(5,3);
+console.log(obj);
+var a= 300;
+console.log(a);
---
+++ lec8nodejs/app.js
@@ -0,0 +1,9 @@
+// var aa=20;
+// var bb=30;
+// console.log(aa+bb);
+
+// console.log(global);
+
+
+console.log(this);
+// console.log(window);
